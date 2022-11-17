6d4a745c76849de9c28f1dfdd1828d465e054f36
Mallikarjuna PV <42732999+pvmallikarjuna@users.noreply.github.com>
2022-11-17 22:26:34 +0530

    handled negative and 0 values

--- area.js
+++ area.js
@@ -5,8 +5,12 @@ const outputEl = document.querySelector("#output");
 
 
 function calcArea() {
-  const totArea = ((Number(sides[0].value)) * (Number(sides[1].value)))/2;
-  outputEl.innerText = "Area of Triangle is : "+ totArea.toFixed(1);
+  if( sides[0].value >= '1' && sides[0].value >= '1'  && sides[0].value >= '1'){
+    const totArea = ((Number(sides[0].value)) * (Number(sides[1].value)))/2;
+    outputEl.innerText = "Area of Triangle is : "+ totArea.toFixed(1);
+  } else {
+    outputEl.innerText = "Please enter positive values.";
+  }
 }
 
 areaBtn.addEventListener("click", calcArea);
