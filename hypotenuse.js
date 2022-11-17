38d110505ec2a72610add0026ef6be1b5b7c20d9
Mallikarjuna PV <42732999+pvmallikarjuna@users.noreply.github.com>
2022-11-17 22:32:57 +0530

    handled negative and 0 values

--- hypotenuse.js
+++ hypotenuse.js
@@ -7,10 +7,13 @@ function calcSquareOf(a, b) {
   return a * a + b * b;
 }
 function calcHypotenuse() {
+  if(  Number(sides[0].value) >= 1 && Number(sides[1].value) >= 1){
   const hypo = Math.sqrt(
-    calcSquareOf(Number(sides[0].value), Number(sides[1].value))
-  ).toFixed(2);
+    calcSquareOf(Number(sides[0].value), Number(sides[1].value))).toFixed(2);
   outputEl.innerText = "Length of Hypotenuse is : "+ hypo;
+}else {
+  outputEl.innerText = "Please enter positive value.";
+}
 }
 
 hypotenuseBtn.addEventListener("click", calcHypotenuse);
